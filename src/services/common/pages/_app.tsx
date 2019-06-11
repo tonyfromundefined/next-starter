bc28f72455937f4e5b7cf8a0b280d104846d4c02
tonyfromundefined <tony@erion.kr>
2019-06-11 13:46:16 +0900

    [refactoring] move _app, _documents to services/common
    add favicon
    add types

---
+++ src/services/common/pages/_app.tsx
@@ -0,0 +1,113 @@
+import ApolloClient from 'apollo-client'
+import pickBy from 'lodash/pickBy'
+import { Container, default as NextApp } from 'next/app'
+import Head from 'next/head'
+import React, { Fragment } from 'react'
+import { ApolloProvider as ApolloHookProvider, getMarkupFromTree } from 'react-apollo-hooks'
+import { renderToString as renderFunction } from 'react-dom/server'
+import { createApolloClient } from '~/apollo'
+import { createStore, IEnvironments, Store, StoreProvider } from '~/store'
+import { ThemeProvider } from '~/styled'
+import GlobalStyle from '~/styled/global'
+import baseTheme from '~/styled/themes/base'
+import FaviconImage from '../assets/favicon.png?url'
+
+const isServer = typeof window === 'undefined'
+
+const environments = isServer ? extractNextEnvironments(process.env) : undefined
+
+export default class extends React.Component {
+  static async getInitialProps(appContext: any) {
+    const appProps = await App.getInitialProps(appContext)
+
+    const { Component, router } = appContext
+
+    const store = createStore({
+      environments,
+    })
+
+    const apollo = createApolloClient(store)
+
+    appContext.ctx.store = store
+
+    if (isServer) {
+      try {
+        await Promise.all([
+          store.nextServerInit(appContext.ctx.req, appContext.ctx.res),
+          getMarkupFromTree({
+            tree: (
+              <App
+                Component={Component}
+                router={router}
+                apolloClient={apollo}
+                store={store}
+                {...appProps}
+              />
+            ),
+            renderFunction,
+          }),
+        ])
+
+      } catch (error) {
+        // tslint:disable-next-line:no-console
+        console.error('[Error 29948] Pre-operation required for SSR failed')
+      }
+
+      Head.rewind()
+    }
+
+    return {
+      apolloState: apollo.cache.extract(),
+      store,
+      ...appProps,
+    }
+  }
+
+  apolloClient: ApolloClient<any>
+  store: Store
+
+  constructor(props: any) {
+    super(props)
+    this.store = createStore(props.store)
+    this.apolloClient = createApolloClient(this.store, props.apolloState)
+  }
+
+  render() {
+    return (
+      <App
+        apolloClient={this.apolloClient}
+        store={this.store}
+        {...this.props}
+      />
+    )
+  }
+}
+
+class App extends NextApp<any> {
+  render() {
+    const { Component, pageProps } = this.props
+
+    return (
+      <Container>
+        <Head>
+          <title>Hello, Next.js</title>
+          <link rel='shortcut icon' href={FaviconImage} />
+        </Head>
+        <ApolloHookProvider client={this.props.apolloClient}>
+          <StoreProvider value={this.props.store}>
+            <ThemeProvider theme={baseTheme}>
+              <Fragment>
+                <Component {...pageProps} />
+                <GlobalStyle />
+              </Fragment>
+            </ThemeProvider>
+          </StoreProvider>
+        </ApolloHookProvider>
+      </Container>
+    )
+  }
+}
+
+function extractNextEnvironments(environments: IEnvironments): IEnvironments {
+  return pickBy(environments, (_value, key) => key.indexOf('NEXT_APP') !== -1)
+}
---
+++ src/types/images.d.ts
@@ -0,0 +1,74 @@
+declare module '*.jpg' {
+  const url: string
+  export default url
+}
+
+declare module '*.svg' {
+  const url: string
+  export default url
+}
+
+declare module '*.png' {
+  const url: string
+  export default url
+}
+
+declare module '*.jpg?include' {
+  const url: string
+  export default url
+}
+
+declare module '*.svg?include' {
+  const url: string
+  export default url
+}
+
+declare module '*.png?include' {
+  const url: string
+  export default url
+}
+
+declare module '*.jpg?webp' {
+  const url: string
+  export default url
+}
+
+declare module '*.svg?webp' {
+  const url: string
+  export default url
+}
+
+declare module '*.png?webp' {
+  const url: string
+  export default url
+}
+
+declare module '*.jpg?inline' {
+  const url: string
+  export default url
+}
+
+declare module '*.svg?inline' {
+  const url: string
+  export default url
+}
+
+declare module '*.png?inline' {
+  const url: string
+  export default url
+}
+
+declare module '*.jpg?url' {
+  const url: string
+  export default url
+}
+
+declare module '*.svg?url' {
+  const url: string
+  export default url
+}
+
+declare module '*.png?url' {
+  const url: string
+  export default url
+}
