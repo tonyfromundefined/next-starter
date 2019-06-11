baf6777ba3bc7ba5cd1f73f49d6aed108e18a9dc
tonyfromundefined <tony@erion.kr>
2019-06-11 13:57:51 +0900

    [feature] serverless-dotenv-plugin
    update index.tsx

--- src/services/home/pages/index.tsx
+++ src/services/home/pages/index.tsx
@@ -9,18 +9,23 @@ export default function PageIndex() {
     <Container>
       <Title>Next.js Starter</Title>
       <Line>
-        <Code>Express.js</Code>
-        <Code>TypeScript</Code>
         <Code>Next.js</Code>
+        <Code>TypeScript</Code>
+        <Code>Babel</Code>
+        <Code>Express.js</Code>
         <Code>Apollo Client</Code>
         <Code>React Apollo</Code>
         <Code>React Apollo Hooks</Code>
         <Code>GraphQL Codegen</Code>
         <Code>MobX</Code>
         <Code>styled-components</Code>
+        <Code>next-optimized-images</Code>
+        <Code>Serverless Framework</Code>
+        <Code>AWS Lambda</Code>
+        <Code>Dotenv</Code>
       </Line>
       <Line>version: {store.environments.NEXT_APP_VERSION}</Line>
-      <Line>by @tonyfromundefined</Line>
+      <Line>by <Author>@tonyfromundefined</Author></Line>
       <Pikachu />
     </Container>
   )
@@ -28,6 +33,16 @@ export default function PageIndex() {
 
 const Container = styled.div`
   padding: 1rem;
+`
+
+const Title = styled.h1`
+  font-size: 2rem;
+  margin: 0 0 1rem;
+`
+
+const Line = styled.div`
+  margin: .5rem 0 0;
+  font-size: .875rem;
 `
 
 const Code = styled.div`
@@ -42,12 +57,14 @@ const Code = styled.div`
   font-weight: 700;
 `
 
-const Title = styled.h1`
-  font-size: 2rem;
-  margin: 0 0 1rem;
-`
-
-const Line = styled.div`
-  margin: .5rem 0 0;
-  font-size: .875rem;
+const Author = styled.div`
+  font-family: monospace;
+  display: inline-block;
+  background-color: ${(props) => props.theme.green[0]};
+  color: ${(props) => props.theme.green[8]};
+  font-size: .75rem;
+  border-radius: .125rem;
+  padding: .0625rem .125rem;
+  margin-right: .25rem;
+  font-weight: 700;
 `
